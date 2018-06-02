b7cca944a6ba1456076b6ed9097d1b470be1d627
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-02 15:32:34 +0200

    Implement straight ranker

--- lib/rankers/straight_ranker.ex
+++ lib/rankers/straight_ranker.ex
@@ -1,10 +1,22 @@
 defmodule PokerHands.Rankers.StraightRanker do
   alias PokerHands.Rankers.HandRanker, as: HandRanker
-  #alias PokerHands.Helpers.CardHelper, as: CardHelper
+  alias PokerHands.Helpers.CardHelper, as: CardHelper
   @behaviour HandRanker
   
   def rank(hand) do
-    {true, hand}
+    hand_order_indexed = CardHelper.get_hand_order_indexed(hand)
+    hand_values = Enum.map(hand_order_indexed, fn(x) -> elem(x, 0) end)
+    hand_values_ordered = Enum.sort(hand_values)
+    
+    if (is_increasing_list(hand_values_ordered, hd(hand_values_ordered))), do: {true, hand}, else: {false, []}
+  end
+
+  def is_increasing_list(list, v) when tl(list) == [] do
+    if hd(list) != v, do: false, else: true
+  end
+
+  def is_increasing_list(list, v) do
+    is_increasing_list(tl(list), v + 1)
   end
 
 end
--- test/rankers/straight_ranker_test.exs
+++ test/rankers/straight_ranker_test.exs
@@ -1,8 +1,9 @@
 defmodule PokerHands.Test.Rankers.StraightRankerTest do
-  #alias PokerHands.Rankers.Straight, as: Straight
+  alias PokerHands.Rankers.StraightRanker, as: StraightRanker
   use ExUnit.Case
 
   test "ranks correcty as straight" do
-    assert true == true
+    hand = [{:"3", :S},{:"2", :S},{:"5", :S},{:"6", :C},{:"4", :C}]
+    assert StraightRanker.rank(hand) == {true, [{:"3", :S},{:"2", :S},{:"5", :S},{:"6", :C},{:"4", :C}]}
   end
 end
