1906da33601585f788662ea5469ea72cb30a56d3
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-02 14:57:04 +0200

    Implement test for flush ranker

--- test/rankers/flush_ranker_test.exs
+++ test/rankers/flush_ranker_test.exs
@@ -1,8 +1,9 @@
 defmodule PokerHands.Test.Rankers.FlushRankerTest do
-  #alias PokerHands.Rankers.FlushRanker, as: FlushRanker
+  alias PokerHands.Rankers.FlushRanker, as: FlushRanker
   use ExUnit.Case
 
   test "ranks correcty as flush" do
-    assert true == true
+    hand = [{:"2", :S},{:"3", :S},{:"4", :S},{:"5", :S},{:"6", :S}]
+    assert FlushRanker.rank(hand) == {true, [{:"2", :S},{:"3", :S},{:"4", :S},{:"5", :S},{:"6", :S}]}
   end
 end
