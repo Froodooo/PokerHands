23fd29d08e1fd79ab4f50aaa8c82262c606997ef
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-02 13:38:20 +0200

    Implement four-of-a-kind ranker

---
+++ lib/rankers/four_of_a_kind_ranker.ex
@@ -0,0 +1,17 @@
+defmodule PokerHands.Rankers.FourOfAKindRanker do
+  alias PokerHands.Rankers.HandRanker, as: HandRanker
+  alias PokerHands.Helpers.CardHelper, as: CardHelper
+  @behaviour HandRanker
+
+  def rank(hand) do
+    sets = CardHelper.get_sets(hand, 4)
+    if Enum.count(sets) == 0 do
+      {false, []}
+    else
+      sets_indices = CardHelper.get_sets_indices(sets)
+      hand_indexed = Enum.with_index(hand)
+      result = CardHelper.get_hand_result(hand_indexed, sets_indices)
+      result
+    end
+  end
+end
---
+++ test/rankers/four_of_a_kind_ranker.exs
@@ -0,0 +1,9 @@
+defmodule PokerHands.Test.Rankers.FourOfAKindRankerTest do
+  alias PokerHands.Rankers.FourOfAKindRanker, as: FourOfAKindRanker
+  use ExUnit.Case
+
+  test "ranks correcty as four of a kind" do
+    hand = [{:"2", :H}, {:"3", :D}, {:"2", :S}, {:"2", :C}, {:"2", :D}]
+    assert FourOfAKindRanker.rank(hand) == {true, [{:"2", :H}, {:"2", :S}, {:"2", :C}, {:"2", :D}, ]}
+  end
+end
