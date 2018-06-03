309792aed823e24c5455cdaaf932a5760573e81d
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-03 19:38:43 +0200

    Implement full house tie function

--- lib/rankers/full_house_ranker.ex
+++ lib/rankers/full_house_ranker.ex
@@ -1,6 +1,7 @@
 defmodule PokerHands.Rankers.FullHouseRanker do
   alias PokerHands.Rankers.HandRanker, as: HandRanker
   alias PokerHands.Helpers.CardHelper, as: CardHelper
+  alias PokerHands.Helpers.RankerHelper, as: RankerHelper
   @behaviour HandRanker
 
   def rank(hand) do
@@ -10,6 +11,24 @@ defmodule PokerHands.Rankers.FullHouseRanker do
   end
 
   def tie(hand_black, hand_white) do
-    {:tie, hand_black, hand_white}
+    hand_black_full_house = elem(hand_black, 1)
+    hand_white_full_house = elem(hand_white, 1)
+
+    IO.puts "test 2342"
+
+    hand_black_suits_3 = CardHelper.get_suit(hand_black_full_house, 3)
+    hand_white_suits_3 = CardHelper.get_suit(hand_white_full_house, 3)
+
+    hand_black_order = List.flatten(Enum.map(hand_black_suits_3, fn(x) -> CardHelper.get_hand_order(x) end))
+    hand_white_order = List.flatten(Enum.map(hand_white_suits_3, fn(x) -> CardHelper.get_hand_order(x) end))
+
+    hand_black_order_sorted = Enum.sort(hand_black_order, &(&1 >= &2))
+    hand_white_order_sorted = Enum.sort(hand_white_order, &(&1 >= &2))
+
+    if (RankerHelper.hands_are_equal(hand_black_order_sorted, hand_white_order_sorted)) do
+      :tie
+    else
+      RankerHelper.compare(hand_black_order_sorted, hand_white_order_sorted)
+    end
   end
 end
