869ea653f582a573f3c99060bed237d7b6351b0b
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-03 19:14:20 +0200

    Implement three/four of a kind tie function

--- lib/rankers/three_of_a_kind_ranker.ex
+++ lib/rankers/three_of_a_kind_ranker.ex
@@ -17,6 +17,19 @@ defmodule PokerHands.Rankers.ThreeOfAKindRanker do
   end
 
   def tie(hand_black, hand_white) do
-    {:tie, hand_black, hand_white}
+    pair_black = elem(hand_black, 1)
+    pair_white = elem(hand_white, 1)
+
+    pair_black_order = CardHelper.get_hand_order_indexed(pair_black)
+    pair_white_order = CardHelper.get_hand_order_indexed(pair_white)
+
+    value_black = elem(Enum.at(pair_black_order, 0), 0)
+    value_white = elem(Enum.at(pair_white_order, 0), 0)
+
+    cond do
+      value_black > value_white -> :black
+      value_white > value_black -> :white
+      true -> :tie
+    end
   end
 end
