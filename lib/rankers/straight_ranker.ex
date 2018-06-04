8f9d5078dbf706d67dc8baca06a0cfe595c474bf
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-04 08:03:30 +0200

    Refactor code related to StraightRanker

--- lib/rankers/straight_ranker.ex
+++ lib/rankers/straight_ranker.ex
@@ -4,40 +4,58 @@ defmodule PokerHands.Rankers.StraightRanker do
   @behaviour HandRanker
 
   def rank(hand) do
-    card_values = CardHelper.get_card_values_indexed(hand)
-    hand_values = Enum.map(card_values, fn x -> elem(x, 0) end)
-    hand_values_ordered = Enum.sort(hand_values)
+    card_values =
+      CardHelper.get_card_values_indexed(hand) |> Enum.map(fn x -> elem(x, 0) end) |> Enum.sort()
 
-    iterations = is_increasing_list(hand_values_ordered, hd(hand_values_ordered), 0)
-    if iterations == Enum.count(hand), do: {true, hand}, else: {false, []}
+    is_increasing_list = is_increasing_list(card_values, hd(card_values), 0)
+
+    rank =
+      if is_increasing_list == Enum.count(hand),
+        do: {true, hand},
+        else: {false, []}
+
+    rank
   end
 
   def tie(hand_black, hand_white) do
-    hand_black_straight = elem(hand_black, 1)
-    hand_white_straight = elem(hand_white, 1)
+    {hand_black_straight, hand_white_straight} = {elem(hand_black, 1), elem(hand_white, 1)}
 
-    black_card_values = CardHelper.get_card_values(hand_black_straight)
-    white_card_values = CardHelper.get_card_values(hand_white_straight)
+    {black_card_values, white_card_values} =
+      {CardHelper.get_card_values(hand_black_straight),
+       CardHelper.get_card_values(hand_white_straight)}
 
-    black_card_values_sorted = Enum.sort(black_card_values, &(&1 >= &2))
-    white_card_values_sorted = Enum.sort(white_card_values, &(&1 >= &2))
+    {black_card_values_sorted, white_card_values_sorted} =
+      {Enum.sort(black_card_values, &(&1 >= &2)), Enum.sort(white_card_values, &(&1 >= &2))}
 
-    value_black = Enum.at(black_card_values_sorted, 0)
-    value_white = Enum.at(white_card_values_sorted, 0)
+    {value_black, value_white} =
+      {Enum.at(black_card_values_sorted, 0), Enum.at(white_card_values_sorted, 0)}
 
-    cond do
-      value_black > value_white -> :black
-      value_white > value_black -> :white
-      true -> :tie
-    end
+    winner =
+      cond do
+        value_black > value_white -> :black
+        value_white > value_black -> :white
+        true -> :tie
+      end
+
+    winner
   end
 
   defp is_increasing_list(list, v, n) when tl(list) == [] do
-    if hd(list) == v, do: n + 1, else: n
+    is_increasing_list =
+      if hd(list) == v,
+        do: n + 1,
+        else: n
+
+    is_increasing_list
   end
 
   defp is_increasing_list(list, v, n) do
-    n = if hd(list) == v, do: n + 1, else: n
-    is_increasing_list(tl(list), v + 1, n)
+    n =
+      if hd(list) == v,
+        do: n + 1,
+        else: n
+
+    is_increasing_list = is_increasing_list(tl(list), v + 1, n)
+    is_increasing_list
   end
 end
