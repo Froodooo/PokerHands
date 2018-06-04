d22a7ec296d2bcfa195dbe821f2edb20a2745396
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-04 19:09:55 +0200

    Implement straight flush tie function

--- lib/rankers/straight_flush_ranker.ex
+++ lib/rankers/straight_flush_ranker.ex
@@ -8,10 +8,17 @@ defmodule PokerHands.Rankers.StraightFlushRanker do
   def rank(hand) do
     is_straight = elem(StraightRanker.rank(hand), 0)
     is_flush = elem(FlushRanker.rank(hand), 0)
-    if is_straight && is_flush, do: {true, hand}, else: {false, hand}
+
+    rank =
+      if is_straight && is_flush,
+        do: {true, hand},
+        else: {false, hand}
+
+    rank
   end
 
   def tie(hand_black, hand_white) do
-    HighCardRanker.tie(hand_black, hand_white)
+    winner = HighCardRanker.tie(hand_black, hand_white)
+    winner
   end
 end
