76414811f9617d9b4d29c065fe659535e7aaed5a
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-10 09:06:51 +0200

    Implement set provider tests

--- lib/helpers/set_provider.ex
+++ lib/helpers/set_provider.ex
@@ -1,6 +1,19 @@
 defmodule PokerHands.Helpers.SetProvider do
   alias PokerHands.Helpers.CardValueProvider, as: CardValueProvider
 
+  @doc ~S"""
+  Gets card sets for a given hand and set size.
+
+  ## Examples
+      iex> PokerHands.Helpers.SetProvider.get_card_sets(["2": :H,"3": :H,"2": :H,"3": :C,K: :D], 2)
+      [[{2, 0}, {2, 2}], [{3, 1}, {3, 3}]]
+
+      iex> PokerHands.Helpers.SetProvider.get_card_sets(["2": :H,"3": :H,"5": :H,"6": :H,"2": :C], 2)
+      [[{2, 0}, {2, 4}]]
+
+      iex> PokerHands.Helpers.SetProvider.get_card_sets(["2": :H,"3": :H,"5": :H,"6": :H,"K": :C], 2)
+      []
+  """
   def get_card_sets(hand, set_size) do
     sets =
       CardValueProvider.get_card_values_indexed(hand)
@@ -11,6 +24,16 @@ defmodule PokerHands.Helpers.SetProvider do
     sets
   end
 
+  @doc ~S"""
+  Gets card indicies for given sets.
+
+  ## Examples
+      iex> PokerHands.Helpers.SetProvider.get_card_sets_indices([[{2, 0}, {2, 2}], [{3, 1}, {3, 3}]])
+      [1, 3]
+
+      iex> PokerHands.Helpers.SetProvider.get_card_sets_indices([[{2, 0}, {2, 4}]])
+      [0, 4]
+  """
   def get_card_sets_indices(sets) do
     highest_card_value = Enum.max(Enum.map(sets, fn x -> elem(hd(x), 0) end))
 
--- test/helpers/set_provider_test.exs
+++ test/helpers/set_provider_test.exs
@@ -1,1 +1,5 @@
-
+defmodule SetProviderTest do
+  alias PokerHands.Helpers.SetProvider, as: SetProvider
+  use ExUnit.Case
+  doctest SetProvider
+end
