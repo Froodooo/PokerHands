46b26cb9ea8c6e4852d7be745896413ca0f0728a
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-10 09:11:05 +0200

    Fix typo

--- lib/helpers/suit_provider.ex
+++ lib/helpers/suit_provider.ex
@@ -1,4 +1,18 @@
 defmodule PokerHands.Helpers.SuitProvider do
+
+  @doc ~S"""
+  Gets suits for a given hand and a suit size.
+
+  ## Examples
+      iex> PokerHands.Helpers.SuitProvider.get_suit(["2": :C,"3": :C,"5": :C,"9": :C,K: :C], 5)
+      [["2": :C,"3": :C,"5": :C,"9": :C,K: :C]]
+
+      iex> PokerHands.Helpers.SuitProvider.get_suit(["2": :C,"3": :C,"5": :C,"9": :D,K: :D], 3)
+      [["2": :C,"3": :C,"5": :C]]
+
+      iex> PokerHands.Helpers.SuitProvider.get_suit(["2": :C,"3": :C,"5": :C,"9": :D,K: :D], 2)
+      [["9": :D,K: :D]]
+  """
   def get_suit(hand, suit_size) do
     suits =
       Enum.group_by(hand, fn x -> elem(x, 1) end)
