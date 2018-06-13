765622d77443b9785bfe752f6f70817e5b12baa4
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-13 21:45:08 +0200

    Refactor two pair ranker

--- lib/rankers/two_pair_ranker.ex
+++ lib/rankers/two_pair_ranker.ex
@@ -6,6 +6,9 @@ defmodule PokerHands.Rankers.TwoPairRanker do
   alias PokerHands.Rankers.HighCardRanker, as: HighCardRanker
   @behaviour HandRanker
 
+  @two_pair_size 2
+  @set_count 2
+
   @doc ~S"""
   Ranks the given hand and returns true if it's a two pair.
 
@@ -14,18 +17,15 @@ defmodule PokerHands.Rankers.TwoPairRanker do
       {true, [{:"2", :H}, {:"3", :D}, {:"2", :S}, {:"3", :C}]}
   """
   def rank(hand) do
-    sets = SetProvider.get_card_value_sets(hand, 2)
+    sets = SetProvider.get_card_value_sets(hand, @two_pair_size)
 
-    rank =
-      if Enum.count(sets) == 2 do
-        sets_indices = get_card_sets_indices(sets)
-        hand_indexed = Enum.with_index(hand)
-        CardValueProvider.get_cards_with_highest_order(hand_indexed, sets_indices)
-      else
-        {false, []}
-      end
-
-    rank
+    if Enum.count(sets) == @set_count do
+      sets_indices = get_card_sets_indices(sets)
+      hand_indexed = Enum.with_index(hand)
+      CardValueProvider.get_cards_with_highest_order(hand_indexed, sets_indices)
+    else
+      {false, []}
+    end
   end
 
   @doc ~S"""
@@ -38,15 +38,14 @@ defmodule PokerHands.Rankers.TwoPairRanker do
       {:white, ["4": :S]}
   """
   def tie(hand_black, hand_white) do
-    highest_pair = get_highest_pair(elem(hand_black, 1), elem(hand_white, 1))
+    {_, black_hand_ranked} = hand_black
+    {_, white_hand_ranked} = hand_white
+    highest_pair = get_highest_pair(black_hand_ranked, white_hand_ranked)
 
-    winner =
-      case highest_pair do
-        {:tie, _} -> HighCardRanker.tie(hand_black, hand_white)
-        _ -> highest_pair
-      end
-
-    winner
+    case highest_pair do
+      {:tie, _} -> HighCardRanker.tie(hand_black, hand_white)
+      _ -> highest_pair
+    end
   end
 
   defp get_highest_pair(hand_black, hand_white) do
@@ -65,21 +64,15 @@ defmodule PokerHands.Rankers.TwoPairRanker do
         do: {:tie, nil},
         else: HandComparer.compare_hands_indexed(black_card_values, white_card_values)
 
-    highest_pair =
-      case rank do
-        :tie -> {:tie, nil}
-        :black -> {:black, [Enum.at(hand_black, highest_pair_index)]}
-        :white -> {:white, [Enum.at(hand_white, highest_pair_index)]}
-      end
-
-    highest_pair
+    case rank do
+      :tie -> {:tie, nil}
+      :black -> {:black, [Enum.at(hand_black, highest_pair_index)]}
+      :white -> {:white, [Enum.at(hand_white, highest_pair_index)]}
+    end
   end
 
   defp get_card_sets_indices(sets) do
-    sets_indices =
-      Enum.flat_map(sets, fn x -> x end)
-      |> Enum.map(fn x -> elem(x, 1) end)
-
-    sets_indices
+    Enum.flat_map(sets, fn x -> x end)
+    |> Enum.map(fn {_, x} -> x end)
   end
 end
