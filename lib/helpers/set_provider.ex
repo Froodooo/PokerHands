3c00fdee3c1116a39591469a5e1ef6837e27b2be
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-13 21:15:39 +0200

    Refactor set provider

--- lib/helpers/set_provider.ex
+++ lib/helpers/set_provider.ex
@@ -2,12 +2,9 @@ defmodule PokerHands.Helpers.SetProvider do
   alias PokerHands.Helpers.CardValueProvider, as: CardValueProvider
 
   def get_card_sets(hand, set_size) do
-    sets =
-      Enum.group_by(hand, fn x -> elem(x, 0) end)
-      |> Map.values()
-      |> Enum.filter(fn x -> Enum.count(x) == set_size end)
-
-    sets
+    Enum.group_by(hand, fn {x, _} -> x end)
+    |> Map.values()
+    |> Enum.filter(fn x -> Enum.count(x) == set_size end)
   end
 
   @doc ~S"""
@@ -24,13 +21,10 @@ defmodule PokerHands.Helpers.SetProvider do
       []
   """
   def get_card_value_sets(hand, set_size) do
-    sets =
-      CardValueProvider.get_card_values_indexed(hand)
-      |> Enum.group_by(fn x -> elem(x, 0) end)
-      |> Map.values()
-      |> Enum.filter(fn x -> Enum.count(x) == set_size end)
-
-    sets
+    CardValueProvider.get_card_values_indexed(hand)
+    |> Enum.group_by(fn {x, _} -> x end)
+    |> Map.values()
+    |> Enum.filter(fn x -> Enum.count(x) == set_size end)
   end
 
   @doc ~S"""
@@ -46,11 +40,8 @@ defmodule PokerHands.Helpers.SetProvider do
   def get_card_sets_indices(sets) do
     highest_card_value = Enum.max(Enum.map(sets, fn x -> elem(hd(x), 0) end))
 
-    sets_indices =
-      Enum.filter(sets, fn x -> elem(hd(x), 0) == highest_card_value end)
-      |> Enum.flat_map(fn x -> x end)
-      |> Enum.map(fn x -> elem(x, 1) end)
-
-    sets_indices
+    Enum.filter(sets, fn x -> elem(hd(x), 0) == highest_card_value end)
+    |> Enum.flat_map(fn x -> x end)
+    |> Enum.map(fn {_, x} -> x end)
   end
 end
