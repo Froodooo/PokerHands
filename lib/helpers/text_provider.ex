5c6c6727ea670e27efb28abfb5b1197774ca06b9
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-13 21:19:45 +0200

    Refactor text provider

--- lib/helpers/text_provider.ex
+++ lib/helpers/text_provider.ex
@@ -27,83 +27,66 @@ defmodule PokerHands.Helpers.TextProvider do
       "ace"
   """
   def get_rank_winner_card_text(rank, winner_cards) do
-    rank_winner_card_text =
-      case rank do
-        :high_card -> get_hand_card_value_text(winner_cards)
-        :single_pair -> get_unique_card_values_text(winner_cards)
-        :two_pair -> get_two_pair_text(winner_cards)
-        :three_of_a_kind -> get_unique_card_values_text(winner_cards)
-        :straight -> get_highest_card_value_text(winner_cards)
-        :flush -> get_highest_card_value_text(winner_cards)
-        :full_house -> get_highest_card_value_text(winner_cards)
-        :four_of_a_kind -> get_unique_card_values_text(winner_cards)
-        :straight_flush -> get_highest_card_value_text(winner_cards)
-      end
-
-    rank_winner_card_text
+    case rank do
+      :high_card -> get_hand_card_value_text(winner_cards)
+      :single_pair -> get_unique_card_values_text(winner_cards)
+      :two_pair -> get_two_pair_text(winner_cards)
+      :three_of_a_kind -> get_unique_card_values_text(winner_cards)
+      :straight -> get_highest_card_value_text(winner_cards)
+      :flush -> get_highest_card_value_text(winner_cards)
+      :full_house -> get_highest_card_value_text(winner_cards)
+      :four_of_a_kind -> get_unique_card_values_text(winner_cards)
+      :straight_flush -> get_highest_card_value_text(winner_cards)
+    end
   end
 
   defp get_card_value_text(card_value) do
-    card_value_text =
-      case card_value do
-        :"2" -> "two"
-        :"3" -> "three"
-        :"4" -> "four"
-        :"5" -> "five"
-        :"6" -> "six"
-        :"7" -> "seven"
-        :"8" -> "eight"
-        :"9" -> "nine"
-        :T -> "ten"
-        :J -> "jack"
-        :Q -> "queen"
-        :K -> "king"
-        :A -> "ace"
-      end
-
-    card_value_text
+    case card_value do
+      :"2" -> "two"
+      :"3" -> "three"
+      :"4" -> "four"
+      :"5" -> "five"
+      :"6" -> "six"
+      :"7" -> "seven"
+      :"8" -> "eight"
+      :"9" -> "nine"
+      :T -> "ten"
+      :J -> "jack"
+      :Q -> "queen"
+      :K -> "king"
+      :A -> "ace"
+    end
   end
 
   defp get_hand_card_value_text(winner_cards) do
-    text =
-      Enum.map(winner_cards, fn x ->
-        "#{get_card_value_text(elem(x, 0))}"
-      end)
-      |> Enum.join(" and ")
-
-    text
+    Enum.map(winner_cards, fn {x, _} ->
+      "#{get_card_value_text(x)}"
+    end)
+    |> Enum.join(" and ")
   end
 
   defp get_highest_card_value_text(winner_cards) do
-    highest_card_value =
+    {_, highest_card_index} =
       CardValueProvider.get_card_values_indexed(winner_cards)
       |> Enum.sort(&(elem(&1, 0) >= elem(&2, 0)))
       |> Enum.at(0)
 
-    highest_card = Enum.at(winner_cards, elem(highest_card_value, 1))
+    {highest_card, _} = Enum.at(winner_cards, highest_card_index)
 
-    text = "#{get_card_value_text(elem(highest_card, 0))}"
-
-    text
+    "#{get_card_value_text(highest_card)}"
   end
 
   defp get_two_pair_text(winner_cards) do
-    text =
-      Enum.map(winner_cards, fn x -> elem(x, 0) end)
-      |> Enum.dedup()
-      |> Enum.map(fn x -> get_card_value_text(x) end)
-      |> Enum.join(" and ")
-
-    text
+    Enum.map(winner_cards, fn {x, _} -> x end)
+    |> Enum.dedup()
+    |> Enum.map(fn x -> get_card_value_text(x) end)
+    |> Enum.join(" and ")
   end
 
   defp get_unique_card_values_text(winner_cards) do
-    text =
-      Enum.map(winner_cards, fn x -> elem(x, 0) end)
-      |> Enum.uniq()
-      |> Enum.map(fn x -> get_card_value_text(x) end)
-      |> Enum.join(" and ")
-
-    text
+    Enum.map(winner_cards, fn {x, _} -> x end)
+    |> Enum.uniq()
+    |> Enum.map(fn x -> get_card_value_text(x) end)
+    |> Enum.join(" and ")
   end
 end
