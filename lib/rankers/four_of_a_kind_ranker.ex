3fe467c7c8560794f062e686292ddf9540f2ca1c
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-13 21:28:13 +0200

    Refactor four of a kind ranker

--- lib/rankers/four_of_a_kind_ranker.ex
+++ lib/rankers/four_of_a_kind_ranker.ex
@@ -3,6 +3,9 @@ defmodule PokerHands.Rankers.FourOfAKindRanker do
   alias PokerHands.Helpers.SetProvider, as: SetProvider
   alias PokerHands.Rankers.ThreeOfAKindRanker, as: ThreeOfAKindRanker
 
+  @four_of_a_kind_size 4
+  @set_count 1
+
   @doc ~S"""
   Ranks the given hand and returns true if it's a four of a kind.
 
@@ -11,19 +14,16 @@ defmodule PokerHands.Rankers.FourOfAKindRanker do
       {true, [{:"2", :H}, {:"2", :S}, {:"2", :C}, {:"2", :D}]}
   """
   def rank(hand) do
-    sets = SetProvider.get_card_value_sets(hand, 4)
+    sets = SetProvider.get_card_value_sets(hand, @four_of_a_kind_size)
 
-    rank =
-      if Enum.count(sets) == 0 do
-        {false, []}
-      else
-        sets_indices = SetProvider.get_card_sets_indices(sets)
-        hand_indexed = Enum.with_index(hand)
-        result = CardValueProvider.get_cards_with_highest_order(hand_indexed, sets_indices)
-        result
-      end
-
-    rank
+    if Enum.count(sets) == @set_count do
+      sets_indices = SetProvider.get_card_sets_indices(sets)
+      hand_indexed = Enum.with_index(hand)
+      result = CardValueProvider.get_cards_with_highest_order(hand_indexed, sets_indices)
+      result
+    else
+      {false, []}
+    end
   end
 
   @doc ~S"""
@@ -36,7 +36,6 @@ defmodule PokerHands.Rankers.FourOfAKindRanker do
       {:white, ["9": :H, "9": :S, "9": :D, "9": :C]}
   """
   def tie(hand_black, hand_white) do
-    winner = ThreeOfAKindRanker.tie(hand_black, hand_white)
-    winner
+    ThreeOfAKindRanker.tie(hand_black, hand_white)
   end
 end
