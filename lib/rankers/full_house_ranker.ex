72e25a293c94b75696083fb1ea6a1c955a87498b
Yordi Verkroost <yordiverkroost@hotmail.com>
2018-06-13 21:31:32 +0200

    Refactor full house ranker

--- lib/rankers/full_house_ranker.ex
+++ lib/rankers/full_house_ranker.ex
@@ -5,6 +5,10 @@ defmodule PokerHands.Rankers.FullHouseRanker do
   alias PokerHands.Helpers.SetProvider, as: SetProvider
   @behaviour HandRanker
 
+  @high_set_size 3
+  @low_set_size 2
+  @set_count 1
+
   @doc ~S"""
   Ranks the given hand and returns true if it's a full house.
 
@@ -13,14 +17,13 @@ defmodule PokerHands.Rankers.FullHouseRanker do
       {true, [{:"2", :S}, {:"2", :D}, {:"2", :H}, {:"5", :C}, {:"5", :D}]}
   """
   def rank(hand) do
-    {sets_3, sets_2} = {SetProvider.get_card_sets(hand, 3), SetProvider.get_card_sets(hand, 2)}
+    {sets_3, sets_2} =
+      {SetProvider.get_card_sets(hand, @high_set_size),
+       SetProvider.get_card_sets(hand, @low_set_size)}
 
-    rank =
-      if Enum.count(sets_3) == 1 && Enum.count(sets_2) == 1,
-        do: {true, hand},
-        else: {false, []}
-
-    rank
+    if Enum.count(sets_3) == @set_count && Enum.count(sets_2) == @set_count,
+      do: {true, hand},
+      else: {false, []}
   end
 
   @doc ~S"""
@@ -33,8 +36,10 @@ defmodule PokerHands.Rankers.FullHouseRanker do
       {:white, [K: :H, K: :S, "4": :H, K: :D, "4": :S]}
   """
   def tie(hand_black, hand_white) do
-    hand_black_sets_3 = SetProvider.get_card_sets(elem(hand_black, 1), 3)
-    hand_white_sets_3 = SetProvider.get_card_sets(elem(hand_white, 1), 3)
+    {_, black_hand_ranked} = hand_black
+    {_, white_hand_ranked} = hand_white
+    hand_black_sets_3 = SetProvider.get_card_sets(black_hand_ranked, @high_set_size)
+    hand_white_sets_3 = SetProvider.get_card_sets(white_hand_ranked, @high_set_size)
 
     black_card_values_sorted =
       Enum.map(hand_black_sets_3, fn x -> CardValueProvider.get_card_values(x) end)
@@ -53,13 +58,10 @@ defmodule PokerHands.Rankers.FullHouseRanker do
         {HandComparer.compare_hands(black_card_values_sorted, white_card_values_sorted), nil}
       end
 
-    result =
-      case winner do
-        {:tie, _} -> winner
-        {:black, _} -> {:black, elem(hand_black, 1)}
-        {:white, _} -> {:white, elem(hand_white, 1)}
-      end
-
-    result
+    case winner do
+      {:tie, _} -> winner
+      {:black, _} -> {:black, black_hand_ranked}
+      {:white, _} -> {:white, white_hand_ranked}
+    end
   end
 end
